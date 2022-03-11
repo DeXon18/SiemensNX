ac536a5c000267ab4490cf20dbb1ca89d1a4584b
DeXon <DeXon18@gmail.com>
2022-03-11 12:05:03 +0100

    Descripciones de las opciones de instalación de NX

--- Instalador NX.bat
+++ Instalador NX.bat
@@ -8,25 +8,63 @@ color 0A
 mode con cols=100 lines=30
 
 :: Descompresion del Zip-y
-echo Descomprimiendo Siemens NX
+echo ################################
+echo ## Descomprimiendo Siemens NX ##
+echo ################################
+
 FOR /F "delims==" %%I IN ('dir /b /s "SiemensNX-*_wntx64.zip"') DO (
 	"7z.exe" x -o"%%~dpnI" "%%I"
 	cd %%~dpnI
 )
 
-REM Limpiamos pantalla
-cls
-
-dir /s
 cls
 REM Instalacion Siemens NX
-echo Instalacion desatencida Siemens NX
+
+REM Las propiedades configurables para la instalación de NX son:
+
+REM Comando LICENSESERVER
+	REM Esta propiedad establece el valor del servidor de licencias NX. Por defecto, será el valor de la variable de entorno
+	REM la variable de entorno SPLM_LICENSE_SERVER (si se ha establecido en una instalación
+	REM instalación anterior) o 28000@<localhost> donde <localhost> es el nombre de su estación de trabajo.
+
+REM Comando LANGUAGE
+	REM Esta propiedad establece el valor del idioma de la interfaz de usuario para NX. Las opciones válidas son
+	REM german, french, spanish, english, italian, japanese, korean, russian, simpl_chinese, trad_chinese, braz_portuguese, czech, hungarian and polish.
+	REM El valor por defecto de esta propiedad es el english.
+
+REM Comando INSTALLDIR
+	REM Esta propiedad establece el directorio donde se instalará NX.
+	REM Si no se especifica "C:\Program Files\Siemens\NX".
+
+REM Comando ADDLOCAL
+	REM Si está realizando una instalación completa, debe especificar ADDLOCAL=ALL.
+	REM Si no se especifica ADDLOCAL en el comando, es lo mismo que ADDLOCAL=FEAT_NXPLATFORM solamente.
+	REM El resto de características de instalación seleccionables para una instalación personalizada son
+	REM FEAT_AUTOMATION_DESIGNER      FEAT_AUTOMOTIVE
+	REM FEAT_DIAGRAMMING              FEAT_DRAFTING
+	REM FEAT_MANUFACTURING_PLANNING   FEAT_MANUFACTURING
+	REM FEAT_MECHATRONICS             FEAT_NXNASTRAN
+	REM FEAT_PROGRAMMING_TOOLS        FEAT_PROGRAMMING_TOOLS
+	REM FEAT_ROUTING                  FEAT_SHIP_BUILDING
+	REM FEAT_SIMULATION               FEAT_STUDIO_RENDER
+	REM FEAT_TOOLING_DESIGN           FEAT_TOOLING_DESIGN
+	REM FEAT_TRANSLATORS              FEAT_VALIDATION
+
+REM TARGETDIR
+	REM Esta propiedad establece el directorio donde se colocará una instalación administrativa de NX administrativa de NX.
+	REM Esta propiedad es sólo para instalaciones administrativas. Consulte Instalación de NX para Parallel Product Testing para más información.
+
+echo ###########################################
+echo ##  Instalacion desatencida Siemens NX   ##
+echo ###########################################
 FOR /F "delims==" %%I IN ('dir /b /s SiemensNX.msi') DO (=
-	msiexec.exe /i %%I ALLUSERS=1 /passive /log output.log ADDLOCAL=FEAT_NXPLATFORM LANGUAGE=spanish SETUPTYPE=custom LICENSESERVER=28000@ServerName
+	msiexec.exe /i %%I ALLUSERS=1 /passive /log output.log LICENSESERVER=28000@<localhost> LANGUAGE=spanish ADDLOCAL=ALL
 )
 
 REM  Instalacion Microsoft Visual C++ Redistributable
-echo Instalacion desatencida Microsoft Visual C++ Redistributable
+echo ##################################################################
+echo ## Instalacion desatencida Microsoft Visual C++ Redistributable ##
+echo ##################################################################
 FOR /F "delims==" %%I IN ('dir /b /s VC_redist.x64.exe') DO (=
 	START /WAIT %%I /Silent
 )
@@ -39,7 +77,6 @@ FOR /F "delims==" %%I IN ('dir /b /s dotnet-runtime-5.0.3-win-x64.exe') DO (=
 
 REM  Instalacion .NET Framework 4.7.2
 REM  En versiones de Windows 10 y 11 no es necesario.
-
 reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v4.7.2" 2>nul
 if errorlevel 1 (
 	echo Instalacion desatencida .NET Framework 4.7.2

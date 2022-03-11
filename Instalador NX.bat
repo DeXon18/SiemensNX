8164284cb0e49dd4ee5dc5bc081bee949ebc57e2
DeXon <DeXon18@gmail.com>
2022-03-11 11:43:45 +0100

    Comprobación de la version net 4.7.2

--- Instalador NX.bat
+++ Instalador NX.bat
@@ -1,7 +1,7 @@
 @echo off
 title Instalacion desatendida Siemens NX Series
 
-:: color fondo codigo letra
+REM color fondo codigo letra
 color 0A
 
 :: Tamaño columnas y lineas
@@ -14,32 +14,45 @@ FOR /F "delims==" %%I IN ('dir /b /s "SiemensNX-*_wntx64.zip"') DO (
 	cd %%~dpnI
 )
 
-:: Limpiamos pantalla
+REM Limpiamos pantalla
 cls
 
 dir /s
 cls
-:: Instalacion Siemens NX
+REM Instalacion Siemens NX
 echo Instalacion desatencida Siemens NX
 FOR /F "delims==" %%I IN ('dir /b /s SiemensNX.msi') DO (=
 	msiexec.exe /i %%I ALLUSERS=1 /passive /log output.log ADDLOCAL=FEAT_NXPLATFORM LANGUAGE=spanish SETUPTYPE=custom LICENSESERVER=28000@ServerName
 )
 
-echo Instalacion desatencida VC_redist.x64.exe
+REM  Instalacion Microsoft Visual C++ Redistributable
+echo Instalacion desatencida Microsoft Visual C++ Redistributable
 FOR /F "delims==" %%I IN ('dir /b /s VC_redist.x64.exe') DO (=
 	START /WAIT %%I /Silent
 )
 
-echo Instalacion desatencida dotnet-runtime-5.0.3-win-x64.exe
+REM  Instalacion .NET 5.0.3
+echo Instalacion desatencida .NET 5.0.3
 FOR /F "delims==" %%I IN ('dir /b /s dotnet-runtime-5.0.3-win-x64.exe') DO (=
 	START /WAIT %%I /Silent
 )
 
+REM  Instalacion .NET Framework 4.7.2
+REM  En versiones de Windows 10 y 11 no es necesario.
 
-echo Instalacion desatencida NDP472-KB4054530-x86-x64-AllOS-ENU.exe
-FOR /F "delims==" %%I IN ('dir /b /s NDP472-KB4054530-x86-x64-AllOS-ENU.exe') DO (=
-	START /WAIT %%I /Silent
+reg query "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\.NETFramework\v4.0.30319\SKUs\.NETFramework,Version=v4.7.2" 2>nul
+if errorlevel 1 (
+	echo Instalacion desatencida .NET Framework 4.7.2
+	FOR /F "delims==" %%I IN ('dir /b /s NDP472-KB4054530-x86-x64-AllOS-ENU.exe') DO (=
+		START /WAIT %%I /Silent
+	)
+) else (
+    echo .NET Framework 4.5.2 is installed
+	pause
 )
+
+
+
 
 pause
 
